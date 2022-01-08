c5361f97093b85279f950f1fb7ce15fec85dfba4
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-01-08 15:39:02 +1000

    Pitch graph lines fixed

--- client/src/components/Kanjium/KanjiumGraph.jsx
+++ client/src/components/Kanjium/KanjiumGraph.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-// Pitch accent graph, 3rd column
+// Pitch accent graph, 3rd JPT column
 const PitchGraph = ({ pattern, graphWidth }) => {
   const array = pattern.split(''),
     length = pattern.length
@@ -10,21 +10,54 @@ const PitchGraph = ({ pattern, graphWidth }) => {
     y = 25
   let x = graphWidth / 2 - (length * (step + radius)) / 2
 
-  let yLast
+  // TODO: include to Point component ?
+  const Path = (props) => {
+    let yLast = y,
+      xPath = x + (step * 3) / 2
+
+    return props.array.map((pitch, i) => {
+      // Default point height for High pitch
+      let cy = y
+
+      // Alter point height if Low pitch
+      if (pitch === 'L') {
+        cy = y + step - radius / 2
+      }
+
+      return (
+        <>
+          {
+            // Exclude 1st point line
+            i !== 0 && (
+              <line
+                x1={xPath}
+                y1={yLast}
+                x2={(xPath += step)}
+                y2={cy}
+                stroke="white"
+                // stroke-dasharray="1, 2"
+                strokeWidth="2"
+              />
+            )
+          }
+          {(yLast = cy)}
+        </>
+      )
+    })
+  }
+
   const Point = (props) => {
-    // Default circle options are for High pitch, altered later if needed
+    // Default point options for High pitch
     let opts = { cy: y, fill: 'white', stroke: '', strokeWidth: '1' }
 
-    // Handling input string: L or H pitch
+    // Alter point options if Low pitch
     if (props.pitch === 'L') {
       opts = { ...opts, cy: y + step - radius / 2 }
     }
 
-    // Handling alternative appearence
-    // (i.e. for the first or the last circle in the graph)
-    if (props.type === 'first') {
-      yLast = opts.cy
-    } else if (props.type === 'last') {
+    // Handling alt appearence
+    // (i.e. first / last points or pitch drop)
+    if (props.type === 'last') {
       opts = {
         ...opts,
         fill: 'transparent',
@@ -36,28 +69,14 @@ const PitchGraph = ({ pattern, graphWidth }) => {
     return (
       <>
         {
-          // Lines are drawn from the second point
-          props.type !== 'first' && (
-            <line
-              x1={x}
-              y1={yLast}
-              x2={x + step / 2}
-              y2={opts.cy}
-              stroke="white"
-              // stroke-dasharray="1, 2"
-              strokeWidth="2"
-            />
-          )
-        }
-        {
           // Last point, triangle
           props.type === 'last' ? (
             <polygon
               points={`
-              ${x + step - radius},${opts.cy - radius}
-              ${x + step + radius},${opts.cy - radius}
-              ${x + step},${opts.cy + radius}
-            `}
+                ${x + step - radius},${opts.cy - radius}
+                ${x + step + radius},${opts.cy - radius}
+                ${x + step},${opts.cy + radius}
+              `}
               fill="transparent"
               stroke="white"
               strokeWidth="1"
@@ -74,34 +93,23 @@ const PitchGraph = ({ pattern, graphWidth }) => {
             />
           )
         }
-        {console.log(`from (${x},${yLast})\nto (${x + step / 2},${opts.cy})`)}
         {(x += step / 2)}
-        {(yLast = opts.cy)}
       </>
     )
   }
 
   return (
     <svg key={pattern._id} width="100%" height="100%">
+      <Path array={array} />
       {array.map((pitch, i, arr) => {
-        // x += step / 2
         let key = `${pattern._id}_${i}`,
           type
-        // First graph point
-        if (i === 0) {
-          type = 'first'
-        }
         // Last graph point
-        else if (i + 1 === arr.length) {
+        if (i + 1 === arr.length) {
           type = 'last'
         }
 
-        return (
-          <>
-            <Point key={key} pitch={pitch} type={type} />{' '}
-            {console.log(`\nendgame\n`)}
-          </>
-        )
+        return <Point key={key} pitch={pitch} type={type} />
       })}
     </svg>
   )
