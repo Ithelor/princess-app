a5d9c3994ea8a1e30bbd00a1d58eff4f647e7913
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-02-05 20:43:00 +1000

    rev: generalized & bugfixes

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,36 +1,19 @@
 import React from 'react'
-import { BsXCircleFill as ExitIcon } from 'react-icons/bs'
 
 import styles from './Modal.module.scss'
 
 interface IModal {
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
-  showExit?: boolean
-  showClose?: boolean
-  title: String
-  content: String
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+  children: JSX.Element | JSX.Element[]
 }
 
 const Modal = (props: IModal) => {
   return (
-    <>
-      <div className={styles.dim} onClick={() => props.setIsOpen(false)}>
-        <div className={styles.container}>
-          <h5 className={styles.heading}>{props.title}</h5>
-          {props.showExit && (
-            <button className={styles.exitBtn} onClick={() => props.setIsOpen(false)}>
-              <ExitIcon />
-            </button>
-          )}
-          <div className={styles.content}>{props.content}</div>
-          {props.showClose && (
-            <button className={styles.closeBtn} onClick={() => props.setIsOpen(false)}>
-              Close
-            </button>
-          )}
-        </div>
+    <div className={styles.dim} onClick={() => props.setIsModalOpen(false)}>
+      <div className={styles.container} onClick={(event) => event.stopPropagation()}>
+        {props.children}
       </div>
-    </>
+    </div>
   )
 }
 
