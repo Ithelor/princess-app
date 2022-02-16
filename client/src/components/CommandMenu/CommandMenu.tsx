a94ba3ec25917472838d6618d85c045b891dc739
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-02-16 18:03:00 +1000

    feat: switched to reducer

--- client/src/components/CommandMenu/CommandMenu.tsx
+++ client/src/components/CommandMenu/CommandMenu.tsx
@@ -15,34 +15,46 @@ import Searchbar from 'components/Searchbar/Searchbar'
 import { IMenuItem, IMainMenu, ISubMenu } from 'interfaces/Menu.interface'
 import { useTheme } from 'hooks/useTheme'
 import { useKeyPress } from 'hooks/useKeyPress'
-// import MenuReducer from 'reducers/menuReducer'
+import MenuReducer from 'reducers/menuReducer'
 
 import styles from './CommandMenu.module.scss'
 
+/*
+ * CommandItem component
+ */
 interface ICommandItem {
   item: IMenuItem
-  current?: boolean
-  active?: boolean
-  onMouseEnter?: React.MouseEventHandler<HTMLLIElement>
+  current: boolean
+  active: boolean
+  onMouseEnter: React.MouseEventHandler<HTMLLIElement>
+  onClick: () => void
 }
 const CommandItem = (props: ICommandItem) => (
-  <li className={classNames({ [styles.active]: props.active })} onMouseEnter={props.onMouseEnter}>
+  <li
+    className={classNames({ [styles.active]: props.active })}
+    onMouseEnter={props.onMouseEnter}
+    onClick={props.onClick}
+  >
     {props.item.icon || <CurrentIcon visibility={props.current ? 'visible' : 'hidden'} />}
     {props.item.name + (props.item.goToMenu ? '...' : '')}
     {props.item.goToMenu && <OpenIcon />}
   </li>
 )
 
+/*
+ * CommandMenu component
+ */
+// FIXME: cannot update a component (`ThemeProvider`) while rendering a different component (`CommandMenu`)
 const CommandMenu = () => {
   const { switchTheme } = useTheme()
 
   const themesMenu: ISubMenu = {
     content: [
-      { id: 1, name: 'dark' },
-      { id: 2, name: 'iceberg_dark' },
-      { id: 3, name: 'laser' },
-      { id: 4, name: 'pulse' },
-      { id: 5, name: 'sonokai' }
+      { id: 0, name: 'dark' },
+      { id: 1, name: 'iceberg_dark' },
+      { id: 2, name: 'laser' },
+      { id: 3, name: 'pulse' },
+      { id: 4, name: 'sonokai' }
     ],
     property: 'theme',
     action: switchTheme
@@ -50,119 +62,67 @@ const CommandMenu = () => {
 
   const mainMenu: IMainMenu = {
     content: [
-      { id: 1, name: 'Theme', icon: <ThemeIcon />, goToMenu: themesMenu },
-      { id: 2, name: 'Option 1', icon: <OptionOneIcon /> },
-      { id: 3, name: 'Option 2', icon: <OptionTwoIcon /> },
-      { id: 4, name: 'Option 3', icon: <OptionThreeIcon /> },
-      { id: 5, name: 'Option 4', icon: <OptionFourIcon /> }
+      { id: 0, name: 'Theme', icon: <ThemeIcon />, goToMenu: themesMenu },
+      { id: 1, name: 'Option 1', icon: <OptionOneIcon /> },
+      { id: 2, name: 'Option 2', icon: <OptionTwoIcon /> },
+      { id: 3, name: 'Option 3', icon: <OptionThreeIcon /> },
+      { id: 4, name: 'Option 4', icon: <OptionFourIcon /> }
     ]
   }
-
-  const [showCommandMenu, setShowCommandMenu] = React.useState(false)
-  const [activeMenu, setActiveMenu] = React.useState(mainMenu)
 
   const escPress = useKeyPress('Escape')
   const upPress = useKeyPress('ArrowUp')
   const downPress = useKeyPress('ArrowDown')
   const enterPress = useKeyPress('Enter')
-
-  // const initialState = { showMenu: false, activeMenu: mainMenu, cursor: 0, current: undefined }
-  // const [state, dispatch] = React.useReducer(MenuReducer, initialState)
-
-  const [cursor, setCursor] = React.useState(0)
   const [hovered, setHovered] = React.useState<IMenuItem>()
-  const [currentOption, setCurrentOption] = React.useState<string | null>()
 
-  // escPress
+  const initialState = { showMenu: false, activeMenu: mainMenu, cursor: 0, current: null }
+  const [state, dispatch] = React.useReducer(MenuReducer, initialState)
+
   React.useEffect(() => {
     if (escPress) {
-      if (!activeMenu.action) {
-        setShowCommandMenu(!showCommandMenu)
-        setCursor(0)
-      } else {
-        activeMenu.action?.call(this, currentOption)
-        setActiveMenu(mainMenu)
-        setCursor(mainMenu.content.findIndex((item) => item.name?.toLowerCase() === activeMenu.property))
-      }
-
-      // dispatch({ type: 'escPress', mainMenu: mainMenu })
+      dispatch({ type: 'ESC_PRESS', payload: { mainMenu: mainMenu } })
     }
   }, [escPress]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  // upPress
   React.useEffect(() => {
-    if (activeMenu.content.length && upPress && showCommandMenu) {
-      const newCursor = cursor > 0 ? cursor - 1 : activeMenu.content.length - 1
-
-      setCursor(newCursor)
-      activeMenu.action?.call(this, activeMenu.content[newCursor].name?.toLowerCase())
-
-      // dispatch({ type: 'upPress' })
+    if (state.activeMenu!.content.length && upPress && state.showMenu) {
+      dispatch({ type: 'UP_PRESS' })
     }
   }, [upPress]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  // downPress
   React.useEffect(() => {
-    if (activeMenu.content.length && downPress && showCommandMenu) {
-      const newCursor = cursor < activeMenu.content.length - 1 ? cursor + 1 : 0
-      setCursor(newCursor)
-      // call action with option name as an argument
-      activeMenu.action?.call(this, activeMenu.content[newCursor].name?.toLowerCase())
-
-      // dispatch({ type: 'downPress' })
+    if (state.activeMenu!.content.length && downPress && state.showMenu) {
+      dispatch({ type: 'DOWN_PRESS' })
     }
   }, [downPress]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  // enterPress
   React.useEffect(() => {
-    if (activeMenu.content.length && enterPress && showCommandMenu) {
-      if (activeMenu.content[cursor].goToMenu) {
-        setActiveMenu(activeMenu.content[cursor].goToMenu!)
-        setCurrentOption(localStorage.getItem(activeMenu.content[cursor].goToMenu!.property!))
-
-        // set current option active
-        setCursor(
-          activeMenu.content[cursor].goToMenu!.content.findIndex((item) => {
-            return item.name === localStorage.getItem(activeMenu.content[cursor].goToMenu!.property!)
-          })
-        )
-      } else if (activeMenu.action) {
-        setActiveMenu(mainMenu)
-
-        // set current menu active
-        setCursor(mainMenu.content.findIndex((item) => item.name?.toLowerCase() === activeMenu.property))
-      }
-
-      // dispatch({ type: 'enterPress' })
+    if (state.activeMenu!.content.length && enterPress && state.showMenu) {
+      dispatch({ type: 'ENTER_PRESS', payload: { mainMenu: mainMenu } })
     }
   }, [enterPress]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  // hovered
   React.useEffect(() => {
-    if (activeMenu.content.length && hovered) {
-      setCursor(activeMenu.content.indexOf(hovered))
-      activeMenu.action?.call(this, hovered.name?.toLowerCase())
-
-      // dispatch({ type: 'hovered', hovered: hovered })
+    if (state.activeMenu!.content.length && hovered) {
+      dispatch({ type: 'HOVERED', payload: { hovered: hovered } })
     }
   }, [hovered]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    showCommandMenu && (
-      <div className={styles.dim} onClick={() => setShowCommandMenu(false)}>
+    state.showMenu && (
+      <div className={styles.dim} onClick={() => dispatch({ type: 'OUTER_CLICK' })}>
         <menu onClick={(event) => event.stopPropagation()}>
           <Searchbar />
           <ul>
-            {activeMenu.content.map((item, index) => (
+            {state.activeMenu!.content.map((item, index) => (
               <CommandItem
                 key={item.id}
                 item={item}
-                active={index === cursor}
-                current={item.name.toLowerCase() === currentOption}
-                onMouseEnter={() => {
-                  setHovered(item)
-                  // dispatch({ type: 'escPress' })
-                }}
+                active={index === state.cursor}
+                current={item.id === state.current}
+                onMouseEnter={() => setHovered(item)}
+                onClick={() => dispatch({ type: 'ENTER_PRESS', payload: { mainMenu: mainMenu } })}
               />
             ))}
           </ul>
