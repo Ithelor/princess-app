86fabfeb53a2e3eae8d3e7cbd13cc419b7038ccb
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-02-18 23:53:49 +1000

    rev: ref and autofocus props

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -4,24 +4,24 @@ import { BsSearch as SearchIcon } from 'react-icons/bs'
 import styles from './Searchbar.module.scss'
 
 interface SearchbarProps {
+  innerRef?: React.RefObject<HTMLInputElement>
+  autoFocus?: boolean
   placeholder?: string
   onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
 
-// nope, no FC.defaultValue 'cause it doesn't re-render props on change
-const defaultProps: SearchbarProps = {
-  placeholder: 'サーチ。。'
-}
-
-const Searchbar = (props: SearchbarProps) => {
-  props = { ...defaultProps, ...props }
-
-  return (
-    <div className={styles.container}>
-      <SearchIcon />
-      <input type="search" autoComplete="off" placeholder={props.placeholder} onChange={props.onChange} />
-    </div>
-  )
-}
+const Searchbar = (props: SearchbarProps) => (
+  <div className={styles.container}>
+    <SearchIcon />
+    <input
+      ref={props.innerRef}
+      autoFocus={props.autoFocus}
+      type="search"
+      autoComplete="off"
+      placeholder={props.placeholder || 'サーチ。。'}
+      onChange={props.onChange}
+    />
+  </div>
+)
 
 export default Searchbar
